3acd5ef32a6638c055f60077783db8bb82b3fc8c
Attila Olbrich <olbrichattila@gmx.co.uk>
2023-07-22 21:45:00 +0100

    Format SQL

--- db_migration_provider.go
+++ db_migration_provider.go
@@ -26,10 +26,13 @@ func (m *DbMigration) LatestMigrations() []string {
 	lastMigrationDate := m.lastMigrationDate()
 
 	rows, err := m.db.Query(`
-		SELECT file_name 
-		FROM migrations
-		WHERE deleted_at is null
-		AND created_at = $1
+		SELECT
+			file_name 
+		FROM
+			migrations
+		WHERE
+			deleted_at is null
+			AND created_at = $1
 	`, lastMigrationDate)
 
 	if err != nil {
@@ -52,7 +55,11 @@ func (m *DbMigration) LatestMigrations() []string {
 
 func (m *DbMigration) AddToMigration(fileName string) error {
 
-	sql := "INSERT INTO migrations (file_name, created_at) values ($1, $2)"
+	sql := `INSERT INTO
+				migrations 
+			(file_name, created_at)
+				values 
+			($1, $2)`
 
 	_, err := m.db.Exec(sql, fileName, m.timeString)
 
@@ -61,11 +68,13 @@ func (m *DbMigration) AddToMigration(fileName string) error {
 }
 
 func (m *DbMigration) RemoveFromMigration(fileName string) error {
-	sql := `
-		UPDATE migrations 
-		SET deleted_at = $1
-		WHERE file_name = $2
-		AND deleted_at IS NULL`
+	sql := `UPDATE
+				migrations 
+			SET
+				deleted_at = $1
+			WHERE
+				file_name = $2
+				AND deleted_at IS NULL`
 
 	_, err := m.db.Exec(sql, m.timeString, fileName)
 
@@ -73,11 +82,14 @@ func (m *DbMigration) RemoveFromMigration(fileName string) error {
 }
 
 func (m *DbMigration) MigrationExistsForFile(fileName string) bool {
-	sql := `
-		SELECT count(*) as cnt
-		FROM migrations
-		WHERE file_name = $2
-		AND deleted_at is null`
+	sql := `SELECT
+				count(*) as cnt
+			FROM
+				migrations
+			WHERE
+				file_name = $2
+			AND
+				deleted_at is null`
 
 	row := m.db.QueryRow(sql, fileName)
 	var count string
@@ -92,12 +104,11 @@ func (m *DbMigration) MigrationExistsForFile(fileName string) bool {
 }
 
 func (m *DbMigration) Init() error {
-	sql := `
-		CREATE TABLE IF NOT EXISTS migrations (
-			file_name VARCHAR(255),
-			created_at VARCHAR(20),
-			deleted_at VARCHAR(20)
-		)`
+	sql := `CREATE TABLE IF NOT EXISTS migrations (
+				file_name VARCHAR(255),
+				created_at VARCHAR(20),
+				deleted_at VARCHAR(20)
+			)`
 
 	_, err := m.db.Exec(sql)
 
@@ -105,10 +116,12 @@ func (m *DbMigration) Init() error {
 }
 
 func (m *DbMigration) lastMigrationDate() string {
-	sql := `
-	SELECT max(created_at) as latest_migration
-	FROM migrations
-	WHERE deleted_at is null`
+	sql := `SELECT
+				max(created_at) as latest_migration
+			FROM
+				migrations
+			WHERE
+				deleted_at is null`
 
 	row := m.db.QueryRow(sql)
 	var maxdate string
